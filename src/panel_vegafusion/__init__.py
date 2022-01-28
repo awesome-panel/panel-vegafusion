0a58b66546250bd7db2b0c3b5c6246290abffc0f
Marc Skov Madsen <masma@orsted.dk>
2022-01-28 07:06:14 +0100

    autoformat

--- src/panel_vegafusion/__init__.py
+++ src/panel_vegafusion/__init__.py
@@ -1,16 +1,19 @@
 import json
-from typing import Union, Optional
+import logging
+from typing import Optional, Union
 
 import altair as alt
 import param
 from panel.reactive import ReactiveHTML
+
 from ._utils import edit_constant, get_chart
-import logging
 
 logger = logging.getLogger("panel-vegafusion")
 
 import vegafusion_jupyter as vf
+
 vf.enable()
+
 
 class VegaFusion(ReactiveHTML):
     """A Panel pane for Altair charts and Vega dictionaries rendered by VegaFusion"""
@@ -20,27 +23,31 @@ class VegaFusion(ReactiveHTML):
 
     verbose = param.Boolean(default=False, doc="""Whether to log or not""")
     indent = param.Integer(2, doc="""The indentation of json specifications""")
-    
+
     debounce_wait = param.Number(30, allow_None=False)
     debounce_max_wait = param.Number(60, allow_None=True)
 
-    spec = param.String(doc="""The Vega json specification derived from the object""", allow_None=True, readonly=True)
+    spec = param.String(
+        doc="""The Vega json specification derived from the object""",
+        allow_None=True,
+        readonly=True,
+    )
     full_vega_spec = param.String(None, allow_None=True, readonly=True)
     client_vega_spec = param.String(None, allow_None=True, readonly=True)
     server_vega_spec = param.String(None, allow_None=True, readonly=True)
     comm_plan = param.String(None, allow_None=True, readonly=True)
-    
+
     download_source_link = param.String(None, allow_None=True)
 
     _template = """
 <div id="view" style="height:100%;width:100%">Hello</div>    
 """
-    
+
     _request = param.String()
     _response = param.String()
 
-    def __init__(self, object: Optional[Union[alt.TopLevelMixin, dict]]=None, **params):
-        self.object=object
+    def __init__(self, object: Optional[Union[alt.TopLevelMixin, dict]] = None, **params):
+        self.object = object
         super().__init__(**params)
         self._update_spec()
 
@@ -82,5 +89,3 @@ class VegaFusion(ReactiveHTML):
 
 if __name__.startswith("bokeh"):
     VegaFusion(object=get_chart()).servable()
-    
-            
